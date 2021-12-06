e5780d03be043745b4571c23a9faa1a9222f01b2
Daniel L <danielkcode@gmail.com>
2021-12-06 15:01:39 -0600

    added signup page template, might extend from index later.

--- financial_tracker/register/views.py
+++ financial_tracker/register/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import login , authenticate
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
 
 def home(request):
     return render(request, "index.html", {})
+
+def registerUser(response):
+    form = UserCreationForm()
+    return render(response, "register/signup.html", {"form":form})
     
