0d4800c0b65232836bebee76f82822abeca62718
Daniel L <danielkcode@gmail.com>
2021-12-06 15:26:10 -0600

    ran migrations and added regusterUser function

--- financial_tracker/register/views.py
+++ financial_tracker/register/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth import login , authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.template import RequestContext
 
 
 def home(request):
     return render(request, "index.html", {})
 
 def registerUser(response):
-    form = UserCreationForm()
-    return render(response, "register/signup.html", {"form":form})
+    # if there is a post request and form is valid
+    if response.method == 'POST':
+        form = UserCreationForm(response.POST)
+        if form.is_valid():
+            # save the form
+            form.save()
+    else:
+        #otherwise create a blank form
+        form = UserCreationForm()
+
+    return render(response, "register/signup.html", {"form":form}, )
     
