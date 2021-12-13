b3f84c8dc7be1c2658da1889b5439c0c6841cb60
Daniel L <danielkcode@gmail.com>
2021-12-12 19:25:11 -0600

    tracker view update

--- financial_tracker/tracker/views.py
+++ financial_tracker/tracker/views.py
@@ -61,14 +61,13 @@ def view_trackers(request):
     current_user = request.user
     total_spent = get_total_spent_info(request)
     dates = get_db_dates(current_user.id)
-    # dates.sort()
     amount_spent = get_expenses(current_user.id)
-    # amount_spent.sort()
-    dates, amount_spent = zip(*sorted(zip(dates, amount_spent)))
+    chart = ""
+    if len(dates) > 0 and len(amount_spent)>0:
+        # if there is data then unpack for the graph
+        dates, amount_spent = zip(*sorted(zip(dates, amount_spent)))
+        chart = get_payment_history(dates, amount_spent)
 
-    chart = get_payment_history(dates, amount_spent)
-
-   
     if FinancialTracker.objects.filter(id=current_user.id).exists():
 
         ft = FinancialTracker.objects.get(id=current_user.id)    
@@ -89,5 +88,3 @@ def view_trackers(request):
             "chart":chart,  
         }
         return render(request, "tracker/tracker_views.html", context)
-
-
